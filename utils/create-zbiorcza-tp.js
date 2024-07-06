d1fe0cf5a687449e55d543de1a62a54b7648ed87
jaroslaw-filipiak <info@j-filipiak.pl>
2024-07-06 15:08:25 +0200

    remove zbiorcz tp unused keys

--- utils/create-zbiorcza-tp.js
+++ utils/create-zbiorcza-tp.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 const Products = require("../models/Products");
 
-const summary = (items) => {
+const summary = (items, keysToKeep) => {
   const grouped = items.reduce((acc, item) => {
     if (!acc[item.range]) {
       acc[item.range] = [];
@@ -23,6 +23,23 @@ const createZBIORCZA_TP = (application) => {
   const m_spiral_sum = summary(application.m_spiral);
   const m_standard_sum = summary(application.m_standard);
   const m_max_sum = summary(application.m_max);
+
+  delete m_spiral_sum[""];
+  delete m_spiral_sum["220-320"];
+  delete m_spiral_sum["320-420"];
+  delete m_spiral_sum["350-550"];
+  delete m_spiral_sum["550-750"];
+  delete m_spiral_sum["750-950"];
+
+  delete m_standard_sum["10-17"];
+  delete m_standard_sum["17-30"];
+  delete m_standard_sum["350-550"];
+  delete m_standard_sum["550-750"];
+  delete m_standard_sum["750-950"];
+
+  delete m_standard_sum["10-17"];
+  delete m_standard_sum["17-30"];
+  delete m_standard_sum["30-50"];
 
   return {
     m_spiral: m_spiral_sum,
